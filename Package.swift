a43c178e7479d64be4d9f1e1c84f9be24d5220ff
AkiyamaAB <wot13321@icloud.com>
2026-06-25 15:46:24 +0300

    init

---
+++ Package.swift
@@ -0,0 +1,51 @@
+// swift-tools-version:6.2
+import PackageDescription
+
+let package = Package(
+    name: "SITAOwebV1",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.121.4"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.13.0"),
+        // 🐬 Fluent driver for MySQL.
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.8.0"),
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.5.1"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.101.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "SITAOwebV1",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "SITAOwebV1Tests",
+            dependencies: [
+                .target(name: "SITAOwebV1"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("InternalImportsByDefault"),
+    .enableUpcomingFeature("MemberImportVisibility"),
+    .enableUpcomingFeature("InferIsolatedConformances"),
+    .enableUpcomingFeature("ImmutableWeakCaptures"),
+] }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SITAOweb
 Web site for Minecraft server SITAO. Built on Swift Vapor
+
+💧 A project built with the Vapor web framework.
+
+## Getting Started
+
+To build the project using the Swift Package Manager, run the following command in the terminal from the root of the project:
+```bash
+swift build
+```
+
+To run the project and start the server, use the following command:
+```bash
+swift run
+```
+
+To execute tests, use the following command:
+```bash
+swift test
+```
